ebcf7e3579729e08c710d4ce25ca9b60a9e09f54
gbugz <george.j.buglewicz@gmail.com>
2024-11-21 18:21:08 -0600

    Creates files for the Adventure Javascript Project

--- src/index.js
+++ src/index.js
@@ -41,17 +41,27 @@ if (TestGrade <= ElementaryGrades) {
 // Write a program where the total purchase amount is stored in a variable. 
 // If the amount is over $50, calculate the discount and log it. Otherwise, log "No discount available."
 
-// FIZZBUZZ
+
+// -----------------------------------------------------------------
+//! FIZZBUZZ
 
 // Run through all the numbers from 1-100
 // Log the number and "FizzBuzz" if the number is divisible by 3 and 5
 // Log the number and "Fizz" if the number is divisible by 3
 // Log the number and "Buzz" if the number is divisible by 5
 // Log the number if none of the above conditions are true
+
+// EXTRA: 
+
+// Modify the FizzBuzz problem to allow the user to choose a max integer.
+// (example: the user is prompted for a number, they choose 15, so the problem runs through the numbers 1 to 15)
+
+
 for (let i = 1; i <= 100; i++) {
     console.log(`${i} ${i%3 === 0 && i%5 ===5}`)
 }
 //TODO: return to this later, review the fizz buzz conditionals and gates
+//TODO: DUE SUNDAY at the latest. Review using zoom recording if absolutely necessary
 
 // EXTRA: 
 
