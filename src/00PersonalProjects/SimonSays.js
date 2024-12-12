36475b5c0394c8ff1173eb02f3561fb16d827c22
Bugz <george.j.buglewicz@gmail.com>
2024-12-11 20:54:29 -0600

    Adds files to work day project. Georgia crashed out mid session, changes are minimal (#25)

--- src/00PersonalProjects/SimonSays.js
+++ src/00PersonalProjects/SimonSays.js
@@ -1,1 +1,9 @@
 //! Decision time: Should I have this all be ONE script sheet? Is that possible?
+
+/*
+export default function SimonSays() {
+  console.log('touch your nose');
+}
+*/
+
+
--- src/00PersonalProjects/WristStretchExercise.js
+++ src/00PersonalProjects/WristStretchExercise.js
@@ -1,2 +1,9 @@
-//*Placeholder
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var myFirstTypedVariable = [1];
+myFirstTypedVariable.push(2);
+var aStringTypedVariable = myFirstTypedVariable.pop().toString();
+var num_or_string = prompt("What do you want?");
+
+
 
